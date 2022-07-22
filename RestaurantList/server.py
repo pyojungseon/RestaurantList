1f782590273cd01db8295d099166aacc6407eb3d
Pyojungseon <suehig@naver.com>
2022-07-22 11:35:41 +0900

    add tag and header treat

--- RestaurantList/server.py
+++ RestaurantList/server.py
@@ -19,13 +19,17 @@ def restaurant():
     content = params['userRequest']['utterance']
     content = content.replace("\n", "")
     tag = "/"
+    header = content.split(" ")[0]
+    if content[0] == '/':
+        tag=header
+
     print(content)
     print(userId)
 
     logData = LogDTO(userId, content, tag)
-    dbCon.insertLogData(self, logData)
+    dbCon.insertLogData(logData)
 
-    if content == "아이디":
+    if header == "아이디":
         print("if문아이디 content in")
         dataSend = {
             "version": "2.0",
@@ -40,7 +44,39 @@ def restaurant():
             }
         }
 
-    elif content == "김소영":
+    elif header == "사용법":
+        print("사용법 content in")
+        dataSend = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "simpleText": {
+                            "text": '''BOK본관 부근 음식점 추천 챗봇입니다.\n
+                            다음의 순서로 입력해주세요\n
+                            추천 종류[한식/일식/중식/양식] 금액대(만원)[N]\n 
+                            ex)추천 일식 5'''
+                        }
+                    }
+                ]
+            }
+        }
+    elif header == "추천":
+        print("추천 content in")
+        dataSend = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "simpleText": {
+                            "text": '''추천테스트. DB만드느중'''
+                        }
+                    }
+                ]
+            }
+        }
+
+    elif header == "김소영":
         print("if문김소영 content in")
         dataSend = {
             "version": "2.0",
@@ -62,7 +98,7 @@ def restaurant():
                 "outputs": [
                     {
                         "simpleText": {
-                            "text": "error 입니다"
+                            "text": "수행할 수 없는 명령문입니다"
                         }
                     }
                 ]

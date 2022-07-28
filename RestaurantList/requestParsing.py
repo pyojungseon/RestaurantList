f256e3efe499ef23106fd9a1c58169204cd74116
Pyojungseon <suehig@naver.com>
2022-07-28 09:40:57 +0900

    modify parameter pasring

--- RestaurantList/requestParsing.py
+++ RestaurantList/requestParsing.py
@@ -17,22 +17,33 @@ class requestParsing:
         header = content.split(" ")[0]
         try:
             tag = params['contexts'][0]['name']
-            lifeSpan = params['contexts'][0]['lifespan']
-            param1 = params['contexts'][0]['param1']
-            param2= params['contexts'][0]['param2']
-            param3 = params['contexts'][0]['param3']
-            param4 = params['contexts'][0]['param4']
-            param5 = params['contexts'][0]['param5']
         except Exception as ex:
             print(ex)
             tag = header
+        try:
+            lifeSpan = params['contexts'][0]['lifespan']
+        except Exception as ex:
             lifeSpan = 0
+        try:
+            param1 = params['contexts'][0]['param1']
+        except Exception as ex:
             param1 = ""
+        try:
+            param2 = params['contexts'][0]['param2']
+        except Exception as ex:
             param2 = ""
+        try:
+            param3 = params['contexts'][0]['param3']
+        except Exception as ex:
             param3 = ""
+        try:
+            param4 = params['contexts'][0]['param4']
+        except Exception as ex:
             param4 = ""
+        try:
+            param5 = params['contexts'][0]['param5']
+        except Exception as ex:
             param5 = ""
-
 
         print(content)
         print(userId)

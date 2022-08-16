6db89b6429c4406a9168daa36bda26175c394cb8
Pyojungseon <suehig@naver.com>
2022-08-16 16:32:50 +0900

    fix param error

--- RestaurantList/requestParsing.py
+++ RestaurantList/requestParsing.py
@@ -37,19 +37,19 @@ class requestParsing:
             tag = header
             lifeSpan = 0
 
-        if not params['action']['clientExtra']['param1']:
+        if params['action']['clientExtra']['param1']:
             param1 = params['action']['clientExtra']['param1']
-        elif not params['contexts'][tagPoint]['params']['param1']['value']:
+        elif params['contexts'][tagPoint]['params']['param1']['value']:
             param1 = params['contexts'][tagPoint]['params']['param1']['value']
 
-        if not params['action']['clientExtra']['param2']:
+        if params['action']['clientExtra']['param2']:
             param2 = params['action']['clientExtra']['param2']
-        elif not params['contexts'][tagPoint]['params']['param2']['value']:
+        elif params['contexts'][tagPoint]['params']['param2']['value']:
             param2 = params['contexts'][tagPoint]['params']['param2']['value']
 
-        if not params['action']['clientExtra']['param3']:
+        if params['action']['clientExtra']['param3']:
             param3 = params['action']['clientExtra']['param3']
-        elif not params['contexts'][tagPoint]['params']['param3']['value']:
+        elif params['contexts'][tagPoint]['params']['param3']['value']:
             param3 = params['contexts'][tagPoint]['params']['param3']['value']
 
         if content=="사용법" or content=="추가" or content=="추천" or content=="수정" or content=="평가" or content=="건의":

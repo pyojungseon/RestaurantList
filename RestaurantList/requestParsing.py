4be01fa4ab26061337313bf56e756a85cb2acccb
Pyojungseon <suehig@naver.com>
2022-08-16 16:49:10 +0900

    fix parsing error

--- RestaurantList/requestParsing.py
+++ RestaurantList/requestParsing.py
@@ -19,12 +19,6 @@ class requestParsing:
         contextSize = len(params['contexts'])
         tagPoint = 0
         lifeSpan = 0
-        param1=""
-        param2=""
-        param3=""
-        param4=""
-        param5=""
-
         tag = ""
         if contextSize>0:
             for i in range(0,contextSize):
@@ -37,20 +31,32 @@ class requestParsing:
             tag = header
             lifeSpan = 0
 
-        if params['action']['clientExtra']['param1']:
+        try:
             param1 = params['action']['clientExtra']['param1']
-        elif params['contexts'][tagPoint]['params']['param1']['value']:
-            param1 = params['contexts'][tagPoint]['params']['param1']['value']
+        except Exception as ex:
+            try :
+                param1 = params['contexts'][tagPoint]['params']['param1']['value']
+            except Exception as ex:
+                param1 = ""
 
-        if params['action']['clientExtra']['param2']:
+        try:
             param2 = params['action']['clientExtra']['param2']
-        elif params['contexts'][tagPoint]['params']['param2']['value']:
-            param2 = params['contexts'][tagPoint]['params']['param2']['value']
+        except Exception as ex:
+            try:
+                param2 = params['contexts'][tagPoint]['params']['param2']['value']
+            except Exception as ex:
+                param2 = ""
 
-        if params['action']['clientExtra']['param3']:
+        try:
             param3 = params['action']['clientExtra']['param3']
-        elif params['contexts'][tagPoint]['params']['param3']['value']:
-            param3 = params['contexts'][tagPoint]['params']['param3']['value']
+        except Exception as ex:
+            try:
+                param3 = params['contexts'][tagPoint]['params']['param3']['value']
+            except Exception as ex:
+                param3 = ""
+
+        param4=""
+        param5=""
 
         if content=="사용법" or content=="추가" or content=="추천" or content=="수정" or content=="평가" or content=="건의":
             tag = content

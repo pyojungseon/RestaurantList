204fa4723d7f05dd06fe4b09437d78337e2c8c20
Pyojungseon <suehig@naver.com>
2022-09-26 16:31:08 +0900

    add carosel

--- RestaurantList/basement.py
+++ RestaurantList/basement.py
@@ -49,7 +49,7 @@ class basement:
                     ]
                 }
             }
-        elif requestDto.header == "사용법":
+        else:
             print("사용법 content in")
             dataSend = {
                 "version": "2.0",
@@ -106,83 +106,6 @@ class basement:
                                                 "messageText": "삭제"
                                             }
                                         ]
-                                    }
-                                ]
-                            }
-                        }
-                    ]
-                },
-                "context": {
-                    "values": [
-                        {
-                            "name": "추가",
-                            "lifeSpan": 0
-                        }, {
-                            "name": "추천",
-                            "lifeSpan": 0
-                        }, {
-                            "name": "수정",
-                            "lifeSpan": 0
-                        }, {
-                            "name": "평가",
-                            "lifeSpan": 0
-                        }, {
-                            "name": "삭제",
-                            "lifeSpan": 0
-                        }, {
-                            "name": "건의",
-                            "lifeSpan": 0
-                        }
-                    ]
-                }
-            }
-        else:
-            print("base if문 content out")
-            dataSend = {
-                "version": "2.0",
-                "template": {
-                    "outputs": [
-                        {
-                            "basicCard": {
-                                "title": "BOK챗봇",
-                                "description": "원하시는 기능을 선택해주세요",
-                                "thumbnail": {
-                                    "imageUrl": ""
-                                },
-                                "profile": {
-                                    "imageUrl": "",
-                                    "nickname": "bok프로필"
-                                },
-                                "buttons": [
-                                    {
-                                        "action": "message",
-                                        "label": "추천",
-                                        "messageText": "추천"
-                                    },
-                                    {
-                                        "action": "message",
-                                        "label": "평가",
-                                        "messageText": "평가"
-                                    },
-                                    {
-                                        "action": "message",
-                                        "label": "추가",
-                                        "messageText": "추가"
-                                    },
-                                    {
-                                        "action": "message",
-                                        "label": "수정",
-                                        "messageText": "수정"
-                                    },
-                                    {
-                                        "action": "message",
-                                        "label": "삭제",
-                                        "messageText": "삭제"
-                                    },
-                                    {
-                                        "action": "message",
-                                        "label": "건의",
-                                        "messageText": "건의"
                                     }
                                 ]
                             }

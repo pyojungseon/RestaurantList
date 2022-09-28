b8995c9905f3d5e1098014f7866a10db51894601
Pyojungseon <suehig@naver.com>
2022-09-28 16:47:00 +0900

    add exception to get thumbnail

--- RestaurantList/getThumbnail.py
+++ RestaurantList/getThumbnail.py
@@ -5,16 +5,21 @@ import requests, re, os
 
 class getThumbnail:
     def getThumbnail(self, name):
-        baseurl="https://search.naver.com/search.naver?where=image&sm=tab_jum&query="
-        url = baseurl + quote_plus(name)
-        html = requests.get(url, verify=False)
-        soup = BeautifulSoup(html.text, 'html.parser')
-        html.close()
+        try:
+            baseurl="https://search.naver.com/search.naver?where=image&sm=tab_jum&query="
+            url = baseurl + quote_plus(name)
+            html = requests.get(url, verify=False)
+            soup = BeautifulSoup(html.text, 'html.parser')
+            html.close()
 
-        data_list = soup.find_all(class_='thumb')
-        thumb = data_list[1].find('img')
-        thumb_src = thumb['src']
-        if thumb_src.find("&type")>-1:
-            thumb_src=thumb_src[0:thumb_src.find("&type")]
+            data_list = soup.find_all(class_='thumb')
+            thumb = data_list[1].find('img')
+            thumb_src = thumb['src']
+            if thumb_src.find("&type")>-1:
+                thumb_src=thumb_src[0:thumb_src.find("&type")]
+        except Exception as ex:
+            print("get thumbnail error")
+            print(ex)
+            return ""
 
         return thumb_src

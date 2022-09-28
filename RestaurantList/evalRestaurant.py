fe342138a5910d0c1865981a84ff9d892d9a03eb
Pyojungseon <suehig@naver.com>
2022-09-28 15:20:20 +0900

    add eval

--- RestaurantList/evalRestaurant.py
+++ RestaurantList/evalRestaurant.py
@@ -13,33 +13,61 @@ class evalRestaurant:
             print("평가 content in")
             lifespan=4
             if requestDto.lifeSpan == 5 or requestDto.lifeSpan == 0:
-                name = requestDto.content.split(" ")[1]
-                if name != None:
+                contents = requestDto.content.split(" ")
+                if len(contents)>1:
+                    name = contents[1]
                     restaurnt = dbCon.getRestaurant(name)
-                    dataSend = {
-                        "version": "2.0",
-                        "template": {
-                            "outputs": [
-                                {
-                                    "simpleText": {
-                                        "text": "가계명 : "+name+" 에 대한 평가멘트를 적어주세요."
+                    if len(restaurnt)>1:
+                        dataSend = {
+                            "version": "2.0",
+                            "template": {
+                                "outputs": [
+                                    {
+                                        "simpleText": {
+                                            "text": "가계명 : "+name+" 에 대한 평가멘트를 적어주세요."
+                                        }
                                     }
-                                }
-                            ]
-                        },
-                        "context": {
-                            "values": [
-                                {
-                                    "name": "평가",
-                                    "lifeSpan": lifespan-1,
-                                    "params": {
-                                        "param1": name,
-                                        "param2": ""
+                                ]
+                            },
+                            "context": {
+                                "values": [
+                                    {
+                                        "name": "평가",
+                                        "lifeSpan": lifespan-1,
+                                        "params": {
+                                            "param1": name,
+                                            "param2": ""
+                                        }
                                     }
-                                }
-                            ]
+                                ]
+                            }
                         }
-                    }
+                    else:
+                        dataSend = {
+                            "version": "2.0",
+                            "template": {
+                                "outputs": [
+                                    {
+                                        "simpleText": {
+                                            "text": "가계명 : "+name+ "이 등록되어있지 않습니다. 평가할 가계명을 다시 입력해주세요"
+                                        }
+                                    }
+                                ]
+                            },
+                            "context": {
+                                "values": [
+                                    {
+                                        "name": "평가",
+                                        "lifeSpan": lifespan,
+                                        "params": {
+                                            "param1": "",
+                                            "param2": ""
+                                        }
+                                    }
+                                ]
+                            }
+                        }
+
                 else:
                     dataSend = {
                         "version": "2.0",
@@ -47,7 +75,7 @@ class evalRestaurant:
                             "outputs": [
                                 {
                                     "simpleText": {
-                                        "text": "가계명 : "+name+" 이 등록되어있지 않습니다. 가계명을 다시 입력해주세요"
+                                        "text": "평가할 가계명을 입력해주세요"
                                     }
                                 }
                             ]

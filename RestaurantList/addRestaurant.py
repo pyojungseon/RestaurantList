ee037207fb4ce41ed3f90f5598481252abb76713
Pyojungseon <suehig@naver.com>
2022-12-29 10:41:18 +0900

    fix

--- RestaurantList/addRestaurant.py
+++ RestaurantList/addRestaurant.py
@@ -18,7 +18,7 @@ class addRestaurant:
                         "outputs": [
                             {
                                 "simpleText": {
-                                    "text": "추가 기능 준비중입니다."
+                                    "text": "(추가 기능 개발중) 음식점 이름을 입력해주세요(이름 중복등록 불가하므로 프렌차이즈는 지점명까지 부탁드립니다)"
                                 }
                             }
                         ]
@@ -27,7 +27,7 @@ class addRestaurant:
                         "values": [
                             {
                                 "name": "추가",
-                                "lifeSpan": 0
+                                "lifeSpan": 4
                             }
                         ]
                     }
@@ -39,7 +39,7 @@ class addRestaurant:
                         "outputs": [
                             {
                                 "simpleText": {
-                                    "text": "2.종류를 입력해주세요(1.한식 / 2.일식 / 3.중식 / 4.양식 / 5.아시안 / 6.랜덤)"
+                                    "text": "2.종류를 입력해주세요\n(숫자로 입력. 1.한식 / 2.일식 / 3.중식 / 4.양식 / 5.아시안)"
                                 }
                             }
                         ]
@@ -57,14 +57,14 @@ class addRestaurant:
                     }
                 }
             elif requestDto.lifeSpan == 3:
-                if int(requestDto.content)>0 or int(requestDto.content)<7:
+                if int(requestDto.content)>0 or int(requestDto.content)<6:
                     dataSend = {
                         "version": "2.0",
                         "template": {
                             "outputs": [
                                 {
                                     "simpleText": {
-                                        "text": "3.금액대(만원)를 입력해주세요(모르거나 애매하면 0)"
+                                        "text": "3.최소금액대(만원)를 입력해주세요"
                                     }
                                 }
                             ]
@@ -89,7 +89,7 @@ class addRestaurant:
                             "outputs": [
                                 {
                                     "simpleText": {
-                                        "text": "입력값 오류. 종류를 다시 입력해주세요"
+                                        "text": "입력값 오류. 다시 입력해주세요\n(숫자로 입력. 1.한식 / 2.일식 / 3.중식 / 4.양식 / 5.아시안)"
                                     }
                                 }
                             ]
@@ -98,9 +98,61 @@ class addRestaurant:
                             "values": [
                                 {
                                     "name": "추가",
-                                    "lifeSpan": 4,
+                                    "lifeSpan": 3,
                                     "params": {
                                         "param1": requestDto.param1
+                                    }
+                                }
+                            ]
+                        }
+                    }
+            elif requestDto.lifeSpan == 2:
+                if int(requestDto.content) > 0 or int(requestDto.content) < 21:
+                    dataSend = {
+                        "version": "2.0",
+                        "template": {
+                            "outputs": [
+                                {
+                                    "simpleText": {
+                                        "text": "4.최대금액대(만원)를 입력해주세요"
+                                    }
+                                }
+                            ]
+                        },
+                        "context": {
+                            "values": [
+                                {
+                                    "name": "추가",
+                                    "lifeSpan": 2,
+                                    "params": {
+                                        "param1": requestDto.param1,
+                                        "param2": requestDto.param2,
+                                        "param3": requestDto.content
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                else:
+                    dataSend = {
+                        "version": "2.0",
+                        "template": {
+                            "outputs": [
+                                {
+                                    "simpleText": {
+                                        "text": "입력값 오류. 다시 입력해주세요"
+                                    }
+                                }
+                            ]
+                        },
+                        "context": {
+                            "values": [
+                                {
+                                    "name": "추가",
+                                    "lifeSpan": 3,
+                                    "params": {
+                                        "param1": requestDto.param1,
+                                        "param2": requestDto.param2
                                     }
                                 }
                             ]
